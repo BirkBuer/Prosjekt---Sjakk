e770ceb7cc74db9f6c3bdafe2dcf287f5533ccd5
Birk Buer <birk.buer@gmail.com>
2022-05-08 15:10:22 +0200

    Moved vector.js content to in-game.js

--- In-game/Javascript/in-game.js
+++ In-game/Javascript/in-game.js
@@ -2,8 +2,6 @@
 const containerEl = document.querySelector(".container");
 const gridEl = document.querySelector(".grid0");
 
-import { VectorFree } from './vector.js';
-
 // Chess board creation
 var switchRow = false;
 for(let x=1; x<=64; x++){
@@ -184,6 +182,68 @@ function loadPieces(){
     };
 }
 
+//Vector/collision checking
+function VectorFree(placementMatrix, y1, x1, y2, x2) {
+    console.log("VectorFree ", x1, y1, x2, y2);
+    if (x1 === x2 && y1 > y2) { //  north
+        console.log('direction is north');
+        for (let i = y1 - 1; i > y2; i--) {
+        if (placementMatrix[i][x1] !== 0) {
+            console.log("VectorFree found an obstacle")
+            return false;
+        }
+        }
+    } else if (x1 < x2 && y1 == y2) { //  east
+        console.log('direction is east');
+        for (let i = x1 + 1; i < x2; i++) {
+        if (placementMatrix[y1][i] !== 0) {
+            console.log("VectorFree found an obstacle")
+            return false;
+        }
+        }
+    } else if (x1 == x2 && y1 < y2) { //  south
+        console.log('direction is south');
+        for (let i = y1 + 1; i < y2; i++) {
+        if (placementMatrix[i][x1] !== 0) {
+            console.log("VectorFree found an obstacle")
+            return false;
+        }
+        }
+    } else if (x1 > x2 && y1 == y2) { //  west
+        console.log('direction is west');
+        for (let i = x1 - 1; i > x2; i--) {
+        if (placementMatrix[y1][i] !== 0) {
+            console.log("VectorFree found an obstacle")
+            return false;
+        }
+        }
+    } else if (x1 < x2 && y1 > y2) { // northeast
+        console.log(`direction is northeast. start position (x,y) is (${x1},${y1})-->(${x2},${y2})`);
+        for (let i = x2 - x1; i > 0; i--) {
+        console.log(`i= ${i}: (x,y) is (${x1 + i},${y1 - i}): ${placementMatrix[x1 + i][y1 - i]}`);
+        if (placementMatrix[x1 + i][y1 - i] !== 0) {
+            console.log("VectorFree found an obstacle")
+            return false;
+        }
+        }
+    } else if (x1 < x2 && y1 < y2) { // southeast
+        console.log(`direction is southeast. start position (x,y) is (${x1},${y1})-->(${x2},${y2})`);
+        for (let i = x1; i > x2; i++) {
+            console.log(`i= ${i}: (x,y) is (${x1 + i},${y1 + i}): ${placementMatrix[x1 + i][y1 + i]}`);
+            if (placementMatrix[x1 + i][y1 + i] !== 0 ) {
+            console.log("VectorFree found an obstacle")
+            return false
+            }
+        }
+    } else if (x1 > x2 && y1 < y2) { // southwest
+        console.log('direction is southwest (not implemented)');
+        return true;
+    } else if (x1 > x2 && y1 > y2) { //  northwest
+        console.log('direction is northwest (not implemented)');
+        return true;
+    }
+    return true;
+}
 //Chess piece movement & selection
 let nodelist = Array.from(containerEl.querySelectorAll("div"));
 console.log(nodelist);
